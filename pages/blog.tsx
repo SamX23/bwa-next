f3dc40b27e5a6d6c308ab63efc710ce5cdde742f
Samx23 <skalexsong@gmail.com>
2021-11-27 22:54:56 +0700

    Update : add serverside on blog page

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,6 +1,38 @@
-import { NextPage } from "next";
 import { Layout } from "../components/Layout";
+import styles from "../styles/Blog.module.css";
 
-const Blog: NextPage = () => <Layout pageTitle="Blog">BLOG</Layout>;
+interface post {
+  id: number;
+  title: string;
+  body: string;
+}
+
+interface props {
+  data: post[];
+}
+
+const Blog = ({ data }: props) => {
+  return (
+    <Layout pageTitle="Blog">
+      {data.map((blog: any, key: any) => (
+        <div className={styles.card} key={key}>
+          <h4>{blog.title}</h4>
+          <p>{blog.body}</p>
+        </div>
+      ))}
+    </Layout>
+  );
+};
 
 export default Blog;
+
+// Will generating data on server for better SEO
+export async function getServerSideProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+  return {
+    props: {
+      data,
+    },
+  };
+}
--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -25,7 +25,7 @@ const UserDetail = ({ user }: props) => {
 
 export default UserDetail;
 
-// Will generating all user detail page by next js on server, faster for client to access it
+// Will generating all user detail page html, faster for client to access it
 export async function getStaticPaths() {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const dataUsers = await res.json();
@@ -46,7 +46,7 @@ interface staticProps {
   params: { id: string };
 }
 
-// Will generating all user detail page by next js on server, faster for client to access it
+// Will generating all user detail page html, faster for client to access it
 export async function getStaticProps(context: staticProps) {
   const { id } = context.params;
   const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
